cc25ee2f58af59b7ad92634ea84d665e09faaf38
johncel <johncel.labajo.rio@gmail.com>
2025-02-04 07:10:35 +0800

    create update delete page

--- src/app/component/AddEmp.tsx
+++ src/app/component/AddEmp.tsx
@@ -1,70 +1,128 @@
-export default function AddEmp() {
+import { useEffect, useState } from "react";
+
+export default function AddEmp({ empData, mode }: { empData: any; mode: any }) {
+  const currentData = empData;
+  console.log(empData);
+  const [empNo, setEmpNo] = useState(currentData?.empNo || "20251");
+  const [fname, setFname] = useState(currentData?.fname || "John");
+  const [mname, setMname] = useState(currentData?.mname || "C.");
+  const [lname, setLname] = useState(currentData?.lname || "Doe");
+  const [dob, setDob] = useState(currentData?.dateofbirth || "");
+  const [maritalStatus, setMaritalStatus] = useState("");
+  const [gender, setGender] = useState(currentData?.gender || "");
+  const [contactNo, setContactNo] = useState(
+    currentData?.contactno || "0987654321"
+  );
+  const [address, setAddress] = useState(currentData?.address || "");
+  const [position, setPosition] = useState(
+    currentData?.position || "Developer"
+  );
+  const [username, setUsername] = useState(currentData?.username || "johnd");
+  const [password, setPassword] = useState(currentData?.password || "pass123");
+
+  useEffect(() => {
+    const currentData2 = currentData;
+    setMaritalStatus(currentData2.maritalstatus);
+  }, [maritalStatus]);
+
   function FormInput({
     id,
     label,
     type,
     classType,
+    value,
   }: {
     id: string;
     label: string;
     type: string;
     classType: string;
+    value: string;
   }) {
+    const [inputValue, setInputValue] = useState(value);
+
     return (
       <div className={`mb-3 ${classType}`}>
         <label htmlFor={id} className="form-label">
           {label}
         </label>
-        <input type={type} name={id} className="form-control" id={id} />
+        <input
+          type={type}
+          name={id}
+          className="form-control"
+          id={id}
+          defaultValue={inputValue ? inputValue : ""}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
       </div>
     );
   }
 
   return (
     <div>
+      {maritalStatus}
       <form className="row">
         <FormInput
           id="fname"
           type="text"
           label="First Name"
           classType={"col-md-4"}
+          value={currentData.fname}
         />
         <FormInput
           id="mname"
           type="text"
           label="Middle Name"
           classType={"col-md-4"}
+          value={currentData.mname}
         />
         <FormInput
           id="lname"
           type="text"
           label="Last Name"
           classType={"col-md-4"}
+          value={currentData.lname}
         />
         <FormInput
           id="dob"
           type="date"
           label="Date of Birth"
           classType={"col-md-3"}
+          value={currentData.dateofbirth}
         />
-         <div className="col-md-2 mb-3" >
-          <label htmlFor="gender" className="form-label">Martital Status</label>
-          <select id="gender" className="form-select" aria-label="Default select example">
-             <option value="1"></option>
-            <option value="1">Maried</option>
-            <option value="2">Single</option>
-            <option value="1">Separated</option>
-            <option value="2">Widowed</option>
-            <option value="1">Divorced</option>
-            <option value="2">Other</option>
+        <div className="col-md-2 mb-3">
+          <label htmlFor="gender" className="form-label">
+            Martital Status
+          </label>
+          <select
+            value={maritalStatus}
+            id="gender"
+            className="form-select"
+            aria-label="Default select example"
+            onChange={(e) => setMaritalStatus(e.target.value)}
+          >
+            <option value="0"></option>
+            <option value="Maried1">Maried</option>
+            <option value="Single">Single</option>
+            <option value="Separated">Separated</option>
+            <option value="Widowed">Widowed</option>
+            <option value="Divorced">Divorced</option>
+            <option value="Other">Other</option>
           </select>
         </div>
-        <div className="col-md-2 mb-3" >
-          <label htmlFor="gender" className="form-label">Gender</label>
-          <select id="gender" className="form-select" aria-label="Default select example">
-           <option value="1"></option>
-            <option value="1">Male</option>
-            <option value="2">Female</option>
+        <div className="col-md-2 mb-3">
+          <label htmlFor="gender" className="form-label">
+            Gender
+          </label>
+          <select
+            value={currentData.gender}
+            defaultValue={0}
+            id="gender"
+            className="form-select"
+            aria-label="Default select example"
+          >
+            <option value="0"></option>
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
           </select>
         </div>
         <FormInput
@@ -72,18 +130,21 @@ export default function AddEmp() {
           type="number"
           label="Contact No"
           classType={"col-md-5"}
+          value={currentData.contactno}
         />
         <FormInput
           id="address"
           type="text"
           label="Address"
           classType={"col-md-6"}
+          value={currentData.address}
         />
         <FormInput
           id="position"
           type="text"
           label="Position"
           classType={"col-md-6"}
+          value={currentData.position}
         />
 
         <div
@@ -101,7 +162,7 @@ export default function AddEmp() {
             Cancel
           </button>
           <button type="submit" className="btn btn-primary">
-            Submit
+            {mode == "edit" ? "Edit" : "Create"}
           </button>
         </div>
       </form>
--- src/app/crud/page.tsx
+++ src/app/crud/page.tsx
@@ -2,41 +2,98 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import AddEmp from "../component/AddEmp";
-import Buttons from "../component/Buttons";
+import { useState } from "react";
 
 export default function CreateUD() {
+  const [empData, setEmpData] = useState({});
+  const [currentMode, setCurrentMode] = useState("");
+  const [employees, setEmployees] = useState([
+    {
+      Empno: "20251",
+      fname: "John Cel",
+      mname: "Labajo",
+      lname: "Rio",
+      username: "John",
+      password: "Ipsumlorem",
+      position: "Developer",
+      contactno: "09893776467",
+    },
+    {
+      Empno: "20252",
+      fname: "Johnsen",
+      mname: "Alquizola",
+      lname: "Sopeta",
+      username: "Bombastic",
+      password: "Ipsumlorem2",
+      position: "Developer",
+      contactno: "09893776467",
+    },
+    {
+      Empno: "20253",
+      fname: "Gilbert",
+      mname: "Nonchalant",
+      lname: "Fuentes",
+      username: "Gilberto",
+      password: "Ipsumlorem1",
+      position: "Developer",
+      contactno: "09893776467",
+    },
+  ]);
+
+  const handleData = (data : any) => {
+    setCurrentMode("edit");
+    let currentData = {
+      Empno: "212",
+      fname: "john",
+      mname: "sopeta",
+      lname: "alquizola",
+      dateofbirth: "july 31, 2000",
+      contactno: "09318481376",
+      address: "barili",
+      position: "developer",
+      gender: "Male",
+      maritalstatus: "Single",
+    };
+    setEmpData(currentData);
+  };
+
+  const handleDelete = (Empno) => {
+    setEmployees((prevEmployees) =>
+      prevEmployees.filter((emp) => emp.Empno !== Empno)
+    );
+  };
+
   return (
-    <div className="crud-maindiv" style={{backgroundColor:'#b3b6b4'}}>
+    <div className="crud-maindiv" style={{ backgroundColor: "#b3b6b4" }}>
+      <div>
       <div>
         <div>
-        <header className="crud-header">
-        <button
-          type="button"
-          className="add"
-          data-bs-toggle="modal"
-          data-bs-target="#exampleModal"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="#ffffff"
-            className="bi bi-plus-circle-fill"
-            viewBox="0 0 16 16"
-            style={{
-              marginLeft: "2px",
-              marginRight: "5px",
-              alignItems: "center",
-              marginBottom: "5px",
-
-            }}
-          >
-            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z" />
-          </svg>
-          Add New Employee
-        </button>
-          <h1 className="ManageEmp">Manage Employees</h1>
-        </header>
+          <header className="crud-header">
+            <button
+              type="button"
+              className="add"
+              data-bs-toggle="modal"
+              data-bs-target="#exampleModal"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="#ffffff"
+                className="bi bi-plus-circle-fill"
+                viewBox="0 0 16 16"
+                style={{
+                  marginLeft: "2px",
+                  marginRight: "5px",
+                  alignItems: "center",
+                  marginBottom: "5px",
+                }}
+              >
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z" />
+              </svg>
+              Add New Employee
+            </button>
+          </header>
         </div>
 
         <div
@@ -48,7 +105,13 @@ export default function CreateUD() {
         >
           <div className="modal-dialog modal-xl" role="document">
             <div className="modal-content px-4">
-              <div style={{ justifyContent: "center", display: "flex", marginTop:'10px' }}>
+              <div
+                style={{
+                  justifyContent: "center",
+                  display: "flex",
+                  marginTop: "10px",
+                }}
+              >
                 <h1>Employee Registration Form</h1>
               </div>
               <div
@@ -58,20 +121,22 @@ export default function CreateUD() {
                   marginBottom: "70px",
                 }}
               >
-                <label htmlFor="emp-info" style={{marginBottom:'-25px'}}>
+                <label htmlFor="emp-info" style={{ marginBottom: "-25px" }}>
                   {" "}
                   Please fill in the information requested below in order to
                   complete the employee registration. Thank You!
                 </label>
               </div>
-              <AddEmp />
+              <AddEmp empData={empData} mode={currentMode} />
             </div>
           </div>
         </div>
       </div>
 
-      <div>
-        <table className="table table-striped table-hover" style={{marginLeft:'40px', marginRight:'40px', width:'96vw'}}>
+        <table
+          className="table table-striped table-hover"
+          style={{ marginLeft: "40px", marginRight: "40px", width: "96vw" }}
+        >
           <thead>
             <tr>
               <th scope="col">#</th>
@@ -87,66 +152,57 @@ export default function CreateUD() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>20251</td>
-              <td>John Cel</td>
-              <td>Labajo</td>
-              <td>Rio</td>
-              <td>John</td>
-              <td>Ipsumlorem</td>
-              <td>Developer</td>
-              <td>09893776467</td>
-              <td><Buttons/></td>
-            </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>20252</td>
-              <td>Johnsen</td>
-              <td>Alquizola</td>
-              <td>Sopeta</td>
-              <td>Bombastic</td>
-              <td>Ipsumlorem2</td>
-              <td>Developer</td>
-              <td>09893776467</td>
-              <td><Buttons/></td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
-              <td>20253</td>
-              <td>Gilbert</td>
-              <td>Nonchalant</td>
-              <td>Fuentes</td>
-              <td>Gilberto</td>
-              <td>Ipsumlorem1</td>
-              <td>Developer</td>
-              <td>09893776467</td>
-              <td><Buttons/></td>
-            </tr>
-            <tr>
-              <th scope="row">4</th>
-              <td>20254</td>
-              <td>John Cel</td>
-              <td>Labajo</td>
-              <td>Rio</td>
-              <td>John</td>
-              <td>Ipsumlorem67</td>
-              <td>Developer</td>
-              <td>09893776467</td>
-              <td><Buttons/></td>
-            </tr>
-            <tr>
-              <th scope="row">5</th>
-              <td>20255</td>
-              <td>Ian Vincent</td>
-              <td>Alquizalas</td>
-              <td>Brufal</td>
-              <td>Ianzkie</td>
-              <td>Ipsumlorem6</td>
-              <td>Developer</td>
-              <td>09893776467</td>
-               <td><Buttons/></td>
-            </tr>
+            {employees.map((employee, index) => (
+              <tr key={employee.Empno}>
+                <th scope="row">{index + 1}</th>
+                <td>{employee.Empno}</td>
+                <td>{employee.fname}</td>
+                <td>{employee.mname}</td>
+                <td>{employee.lname}</td>
+                <td>{employee.username}</td>
+                <td>{employee.password}</td>
+                <td>{employee.position}</td>
+                <td>{employee.contactno}</td>
+                <td>
+                <button
+                  type="button"
+                  data-bs-toggle="modal"
+                  data-bs-target="#exampleModal"
+                  data-bs-whatever="@mdo"
+                  onClick={(e) => handleData(employee)}
+                  className="edit-button"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="currentColor"
+                    className="bi bi-pencil-square"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                    <path d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
+                  </svg>
+                </button>
+                  <button
+                    type="button"
+                    className="delete-button"
+                    onClick={() => handleDelete(employee.Empno)}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      fill="currentColor"
+                      className="bi bi-trash3-fill"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5" />
+                    </svg>
+                  </button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
