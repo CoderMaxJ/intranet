f93d0bc713f56c0c1cab161f9216bd1f953cda31
johnsen <johnsensopeta555@gmail.com>
2025-02-27 10:20:23 +0800

    done merge

--- src/app/Updatepassword/updatepassword.tsx
+++ src/app/Updatepassword/updatepassword.tsx
@@ -25,6 +25,7 @@ export default function Updatepassword() {
   const [showPassword2, setShowPassword2] = useState(false);
   const [hovered, setHovered] = useState(false);
   const [passwordStrength,setPasswordStrength]=useState(Boolean);
+  const [focus,setFocus]=useState(false);
 
 
   const toggleShow = () => {
@@ -166,7 +167,7 @@ const btnClose = document.getElementById("btn-close");
       >
         <div className="modal-dialog">
           <div className="modal-content">
-  
+        
             <div className="modal-header">
               <img
                 src="/img/Sos.png"
@@ -175,6 +176,7 @@ const btnClose = document.getElementById("btn-close");
                 id="updatePasswordModalLabel"
                 style={{ height: '50px', marginLeft: '60px' }}
               ></img>
+
               <button
                 type="button"
                 id = "btn-close"
@@ -201,15 +203,7 @@ const btnClose = document.getElementById("btn-close");
               </div>
             ) }
             <div className="modal-body" style={{ marginLeft: "35px" }}>
-            <label style={{fontSize:"13px"}}>
-  Password must be at least 8 characters and include:
-  <ul>
-    <li style={{color:"red"}}>One lowercase letter</li>
-    <li style={{color:"red"}}>One uppercase letter</li>
-    <li style={{color:"red"}}>One number</li>
-    <li style={{color:"red"}}>One special character (@$!%*?&)</li>
-  </ul>
-</label>
+            
               <form onSubmit={handleSubmit}>
                 <div className="updatepass-label">
                   <label htmlFor="currentpassword">Current Password</label>
@@ -221,13 +215,6 @@ const btnClose = document.getElementById("btn-close");
                       onChange={(e) => setCurrentPassword(e.target.value)}
                       required
                     />
-                    {currentpassword && (
-                      <button
-                        type="button"
-                        onClick={toggleShow}
-                        className="cp-button"
-                      ></button>
-                    )}
                   </div>
                 </div>
 
@@ -235,21 +222,29 @@ const btnClose = document.getElementById("btn-close");
                   <label htmlFor="password">New Password {password != "" && (<span style={{color: passwordStrength === true ? "green":"red",fontSize:"13px",marginLeft:"80px"}}>{passwordStrength === true ? "Strong password": "Weak password"}</span>)}</label>
                   <div style={{ position: "relative" }}>
                     <input
+                      onFocus={()=>setFocus(true)}
                       onKeyUp={validate}
-                      className="updatepassword-input"
+                      className="updatepassword-input1"
                       type={showPassword1 ? "text" : "password"}
                       value={password}
                       onChange={(e) => setPassword(e.target.value)}
                       required
                     />
-                    {password && (
-                      <button
-                        type="button"
-                        onClick={toggleShoww}
-                        className="ps-button"
-                      ></button>
+                   <i className="bi bi-check"></i>
+                    {focus === true && (
+                      <div>
+                           <label htmlFor="" style={{fontSize:"13px",color: passwordStrength === true ? "green":"grey"}}> 
+                           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-check" viewBox="0 0 16 16">
+  <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425z"/>
+</svg>
+                            </label> <label htmlFor="" style={{fontSize:"13px",marginTop:"-45px",color: passwordStrength === true ? "green":"gray"}}>8 to 20 characters </label><br />
+                           <label  htmlFor="" style={{fontSize:"13px",marginBottom:"10px" , color:passwordStrength === true ? "green":""}}> Letters,numbers, and special characters</label>
+                      </div>
+        
                     )}
+         
                   </div>
+              
                 </div>
 
                 <div className="updatepass-label">
@@ -257,19 +252,12 @@ const btnClose = document.getElementById("btn-close");
                   <div style={{ position: "relative" }}>
                     <input
                       className="updatepassword-input"
- 
+                      onFocus={()=>setFocus(false)}
                       type={showPassword2 ? "text" : "password"}
                       value={confirmPassword}
                       onChange={(e) => setConfirmPassword(e.target.value)}
                       required
                     />
-                    {confirmPassword && (
-                      <button
-                        type="button"
-                        onClick={toggleShowww}
-                        className="cnfrm-button"
-                      ></button>
-                    )}
                   </div>
                 </div>
 
