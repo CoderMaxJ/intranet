98c9a073fbf3be22848333d4d268ffc18570b4c9
johncel <johncel07312000@gmail.com>
2025-04-10 04:35:08 +0800

    final updates

--- src/app/Dashboard/dashboard.tsx
+++ src/app/Dashboard/dashboard.tsx
@@ -194,16 +194,16 @@ export default function Dashboard() {
                     )}
                 </div>
                 <div style={{ overflow: 'hidden' }}>
-                    <div className="generate">
-                        <button id="dashboard" className={`nav-font ${navWidth === '217px' ? 'hide-icon-name' : 'show-icon-name'}`} onClick={() => routerPush("/WorkforceMonitoring")}>
+                    <div className="generate"  onClick={() => routerPush("/WorkforceMonitoring")}>
+                        <button id="dashboard" className={`nav-font ${navWidth === '217px' ? 'hide-icon-name' : 'show-icon-name'}`}>
                             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-grid" viewBox="0 0 16 16" style={{ marginRight: '24px' }}>
                                 <path d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5zM2.5 2a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5zm6.5.5A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5zM1 10.5A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5zm6.5.5A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5z" />
                             </svg>
                             {showDashboard === true && (<label className="dash" htmlFor="dashboardd">Dashboard</label>)}
                         </button>
                     </div>
-                    <div className="generate text-dark">
-                        <a className="nav-font " onClick={() => routerPush("/Reports")} style={{ color: '#ffffff' }}>
+                    <div className="generate text-dark" onClick={() => routerPush("/Reports")}>
+                        <a className="nav-font " style={{ color: '#ffffff' }}>
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 width="16"
